4c16d1398f173470e5a88d4293aae96bd3c06276
Kriti Dutta <69663702+duttakriti@users.noreply.github.com>
2021-04-21 09:10:12 +0530

    Update Student.java

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -5,41 +5,41 @@ import org.springframework.data.annotation.Id;
 public class Student {
 
     @Id
-    private Integer regNumber;
-    private String firstName;
-    private String lastName;
+    private Integer invoicenum;
+    private String seller;
+    private String buyer;
 
     public Student() {
     }
 
-    public Student(Integer regNumber, String firstName,  String lastName) {
-        this.regNumber = regNumber;
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Student(Integer invoicenum, String seller, String buyer) {
+        this.invoicenum = invoicenum;
+        this.seller = seller;
+        this.buyer = buyer;
     }
 
     public Integer getRegNumber() {
-        return regNumber;
+        return invoicenum;
     }
 
-    public void setRegNumber(Integer regNumber) {
-        this.regNumber = regNumber;
+    public void setRegNumber(Integer invoicenum) {
+        this.invoicenum = invoicenum;
     }
 
     public String getFirstName() {
-        return firstName;
+        return seller;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String seller) {
+        this.seller = seller;
     }
 
     public String getLastName() {
-        return lastName;
+        return buyer;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName(String buyer) {
+        this.buyer = buyer;
     }
 
 }
